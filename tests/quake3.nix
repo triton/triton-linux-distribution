NixOS/nixpkgs

commit 6b447a3c9bbee41389052d6fa9e08a53d8447656

quake3demodata: split from point release, cleanup

pkgs/games/quake3/content/demo.nix
@@ -1,0 +1,28 @@
+{ stdenv, fetchurl }:
+
+let
+  version = "1.11-6";
+in stdenv.mkDerivation {
+  name = "quake3-demodata-${version}";
+
+  src = fetchurl {
+    url = "http://ftp.gwdg.de/pub/misc/ftp.idsoftware.com/idstuff/quake3/linux/linuxq3ademo-${version}.x86.gz.sh";
+    sha256 = "1v54a1hx1bczk9hgn9qhx8vixsy7xn7wj2pylhfjsybfkgvf7pk4";
+  };
+
+  buildCommand = ''
+    tail -n +165 $src | tar xfz -
+
+    mkdir -p $out/baseq3
+    cp demoq3/*.pk3 $out/baseq3
+  '';
+
+  preferLocalBuild = true;
+
+  meta = with stdenv.lib; {
+    description = "Quake 3 Arena demo content";
+    license = licenses.unfreeRedistributable;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ abbradar ];
+  };
+}

pkgs/games/quake3/demo/builder.sh
@@ -1,9 +1,0 @@
-source $stdenv/setup
-
-tail -n +165 $demo | tar xvfz -
-chmod -R +w .
-tail -n +175 $update | tar xvfz -
-chmod -R +w .
-
-mkdir -p $out/baseq3
-cp demoq3/*.pk3 baseq3/*.pk3 $out/baseq3

pkgs/games/quake3/demo/default.nix
@@ -1,24 +1,0 @@
-{stdenv, fetchurl}:
-
-stdenv.mkDerivation {
-  name = "quake3demo-1.11-6";
-  builder = ./builder.sh;
-
-  # This is needed for pak0.pk3.
-  demo = fetchurl {
-    url = http://tarballs.nixos.org/linuxq3ademo-1.11-6.x86.gz.sh;
-    sha256 = "1v54a1hx1bczk9hgn9qhx8vixsy7xn7wj2pylhfjsybfkgvf7pk4";
-  };
-
-  # This is needed for the additional pak?.pk3 files.
-  update = fetchurl {
-    url = http://tarballs.nixos.org/linuxq3apoint-1.31.x86.run;
-    sha256 = "1kp689452zb8jhd67ghisz2055pqxy9awz4vi0hq5qmp7xrp1x58";
-  };
-
-  # Don't rebuild if the inputs change, since the output is guaranteed
-  # to be this value.
-  outputHashMode = "recursive";
-  outputHashAlgo = "sha256";
-  outputHash = "00453c43a4jnlbm9w9ws1hdi28hkl63xnxbnbqml25h35ckhzs90";
-}

pkgs/top-level/all-packages.nix
@@ -14260,10 +14260,10 @@ let
     name = "quake3-demo-${ioquake3.name}";
     description = "Demo of Quake 3 Arena, a classic first-person shooter";
     game = ioquake3;
-    paks = [quake3demodata];
+    paks = [ quake3pointrelease quake3demodata ];
   };
 
-  quake3demodata = callPackage ../games/quake3/demo { };
+  quake3demodata = callPackage ../games/quake3/content/demo.nix { };
 
   quake3pointrelease = callPackage ../games/quake3/content/pointrelease.nix { };
 

triton/triton-linux-distribution

commit 803321511b2e56221943efb6c602fcc97d473772

nixos/tests/quake3: Fix eval due to unfree license
Regression introduced by 6b447a3c9bbee41389052d6fa9e08a53d8447656.

In this commit the Quake 3 demo data now have a meta attribute which
specifies the license as unfreeRedistributable.

While I haven't found anything official about that on the web, let's
just allow it to be used in the test because first of all, we have been
using it for a long time (since 2009, introduced in d534d44) and second,
because it will be quite some effort to rewrite the test with something
like OpenArena (particularily because we need coverage data and need to
use the ioquake3 version plus OpenArena-specific patches).

Tested evaluation on my local system, but the VM test still fails.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

tests/quake3.nix
@@ -10,6 +10,13 @@ let
       });
     };
 
+  # Only allow the demo data to be used (only if it's unfreeRedistributable).
+  unfreePredicate = pkg: with pkgs.lib; let
+    allowDrvPredicates = [ "quake3-demo" "quake3-pointrelease" ];
+    allowLicenses = [ pkgs.lib.licenses.unfreeRedistributable ];
+  in any (flip hasPrefix pkg.name) allowDrvPredicates &&
+     elem (pkg.meta.license or null) allowLicenses;
+
 in
 
 rec {
@@ -28,6 +35,7 @@ rec {
       hardware.opengl.driSupport = true;
       environment.systemPackages = [ pkgs.quake3demo ];
       nixpkgs.config.packageOverrides = overrides;
+      nixpkgs.config.allowUnfreePredicate = unfreePredicate;
     };
 
   nodes =
@@ -41,6 +49,7 @@ rec {
                 "'+map q3dm7' '+addbot grunt' '+addbot daemia' 2> /tmp/log";
             };
           nixpkgs.config.packageOverrides = overrides;
+          nixpkgs.config.allowUnfreePredicate = unfreePredicate;
           networking.firewall.allowedUDPPorts = [ 27960 ];
         };
 